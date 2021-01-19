5c05910764911261b7bae2916f561cf88f0cb955
2017030446 <2017030446@hanyang.ac.kr>
2021-01-20 03:37:06 +0900

    minor updates from master

---
+++ app/src/main/java/com/example/bapplusplus/TestPaint.kt
@@ -0,0 +1,11 @@
+package com.example.bapplusplus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TestPaint : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_paint)
+    }
+}
