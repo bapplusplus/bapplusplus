0e337e9ed402b52d36c8ea5a8bac642faaa4d69c
2017030446 <2017030446@hanyang.ac.kr>
2021-01-20 03:47:31 +0900

    small updates

--- app/src/main/java/com/example/bapplusplus/Roulette.kt
+++ app/src/main/java/com/example/bapplusplus/Roulette.kt
@@ -1,5 +1,10 @@
 package com.example.bapplusplus
 
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +12,11 @@ class Roulette : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_roulette)
+
+
+        fun onDraw(canvas: Canvas){
+
+        }
+
     }
 }
