1897b6b0841cf2141bd206f3c6bffd7f3fe0b241
SangYupHan <gtd9872@gmail.com>
2021-01-21 22:36:52 +0900

    roulette update

--- app/src/main/java/com/example/bapplusplus/Roulette.kt
+++ app/src/main/java/com/example/bapplusplus/Roulette.kt
@@ -1,21 +1,27 @@
 package com.example.bapplusplus
 
-import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Paint
-import android.graphics.Color
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.activity_roulette.*
+import java.util.*
 
 class Roulette : AppCompatActivity() {
+
+    val random = Random()
+
+    fun rand(from: Int, to: Int) : Int {
+        return random.nextInt(to - from) + from
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_roulette)
 
+//        roulette_view.layoutParams = LinearLayout.LayoutParams(500, 500)
 
-        fun onDraw(canvas: Canvas){
-
+        btn_rotate.setOnClickListener {
+            roulette_view.rotate(roulette_view, 0f, rand(3600, 3960).toFloat())
         }
 
     }
